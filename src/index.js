d337abf1df90d01f1b63b52dcb802762ea3c87ea
peevaliz <107015027+peevaliz@users.noreply.github.com>
2022-07-30 23:06:21 -0700

    Implemented the unit convertion

--- src/index.js
+++ src/index.js
@@ -63,14 +63,13 @@ function changeCity(event) {
   axios.get(`${url}`).then(showTempetature);
 
   function showTempetature(response) {
-    console.log(response.data);
+    celsiusTemperature = response.data.main.temp;
+
     let temperature = Math.round(response.data.main.temp);
-    console.log(temperature);
     let currentTemp = document.querySelector("#current-temp");
-    currentTemp.innerHTML = `${temperature}`;
+    currentTemp.innerHTML = `${temperature}°C`;
 
     let feelsLikeData = Math.round(response.data.main.feels_like);
-    console.log(feelsLikeData);
     let feelsLike = document.querySelector("#feels-like");
     feelsLike.innerHTML = `${feelsLikeData}°C`;
 
@@ -83,7 +82,6 @@ function changeCity(event) {
     wind.innerHTML = `${windData} km/h`;
 
     let icon = document.querySelector("#icon");
-    console.log(response.data.weather[0].icon);
     icon.setAttribute("src", `src/${response.data.weather[0].icon}.png`);
     icon.setAttribute("alt", response.data.weather[0].description);
   }
@@ -94,35 +92,50 @@ cityForm.addEventListener("submit", changeCity);
 
 //Celsius to farenheit
 
-function toFarenheit() {
-  let temperature = document.querySelector(".temp");
-  temperature.innerHTML = 65;
+function toFarenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temp");
+  let fahrenheilTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = `${Math.round(fahrenheilTemperature)}°F`;
+
+  let feelsLike = document.querySelector("#feels-like");
+  feelsLike.innerHTML = `${Math.round(fahrenheilTemperature)}°F`;
+
+  farenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
 }
 
 let farenheitLink = document.querySelector("#farenheit-link");
 farenheitLink.addEventListener("click", toFarenheit);
 
-function toCelsius() {
-  let temperature = document.querySelector(".temp");
-  temperature.innerHTML = 12;
+function toCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#current-temp");
+  temperature.innerHTML = `${Math.round(celsiusTemperature)}°C`;
+
+  let feelsLike = document.querySelector("#feels-like");
+  feelsLike.innerHTML = `${Math.round(celsiusTemperature)}°C`;
+
+  farenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
 }
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", toCelsius);
 
+let celsiusTemperature = null;
+
 function showTempetature(response) {
-  console.log(response.data);
+  celsiusTemperature = response.data.main.temp;
+
   let temperature = Math.round(response.data.main.temp);
   let currentCity = response.data.name;
-  console.log(currentCity);
-  console.log(temperature);
   let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = `${temperature}`;
+  currentTemp.innerHTML = `${temperature}°C`;
   let h1 = document.querySelector("h1");
   h1.innerHTML = currentCity;
 
   let feelsLikeData = Math.round(response.data.main.feels_like);
-  console.log(feelsLikeData);
   let feelsLike = document.querySelector("#feels-like");
   feelsLike.innerHTML = `${feelsLikeData}°C`;
 
@@ -135,7 +148,6 @@ function showTempetature(response) {
   wind.innerHTML = `${windData} km/h`;
 
   let icon = document.querySelector("#icon");
-  console.log(response.data.weather[0].icon);
   icon.setAttribute("src", `src/${response.data.weather[0].icon}.png`);
   icon.setAttribute("alt", response.data.weather[0].description);
 }
@@ -143,8 +155,6 @@ function showTempetature(response) {
 function showPosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  console.log(lat);
-  console.log(lon);
   let units = "metric";
   let apiKey = `76a35369f06f76080a05f6fc077ecb04`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
